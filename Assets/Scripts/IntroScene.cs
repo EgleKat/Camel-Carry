2e26ec739ae9c9caa6dc95eb20e34fe6a7b04731
Unknown <ek1g15@soton.ac.uk>
2018-02-20 12:45:46 +0000

    Renamed Controller to IntroScene

--- Assets/Scripts/IntroScene.cs
+++ Assets/Scripts/IntroScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Controller : MonoBehaviour {
+public class IntroScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
