e7d5aaa5fcbfe2c987737408492cdb07fc32e470
tedyeates <te3d.y34t3s@gmail.com>
2018-02-20 20:01:27 +0100

    Did Inventory Stuff
    Cried a lot

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    public List<GameObject> inventory;
+    public List<GameObject> camelInventory;
+
+	private int currentInventorySize;
+	private int currentCamelInventorySize;
+
+	public Moving camelMoving;
+
+
+    // Use this for initialization
+    void Start()
+    {
+		currentCamelInventorySize = 0;
+		currentInventorySize = inventory.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Removes item from inventory and puts it in camel
+    private void MoveItemToCamel(GameObject object_to_move)
+    {
+        if (currentCamelInventorySize >= camelInventory.Count)
+            return;
+
+        inventory.Remove(object_to_move);
+        camelInventory.Add(object_to_move);
+
+    }
+
+    // Removes item from camel and puts it in inventory
+    private void MoveItemToInventory(GameObject objectToMove)
+    {
+        if (currentInventorySize >= inventory.Count)
+            return;
+
+        camelInventory.Remove(objectToMove);
+        inventory.Add(objectToMove);
+
+		currentInventorySize++;
+
+    }
+
+
+    public void ToggleInventory(GameObject object_to_move)
+    {
+        if (inventory.Contains(object_to_move))
+        {
+            MoveItemToCamel(object_to_move);
+        }
+        else
+        {
+            MoveItemToInventory(object_to_move);
+        }
+    }
+
+	public void SellItems()
+	{
+		camelMoving.StartMoving ();
+	}
+}
