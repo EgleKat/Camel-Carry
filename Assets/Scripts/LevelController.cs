e6fe46177aa2214800ade6693ee504ff4bf052d4
Unknown <ek1g15@soton.ac.uk>
2018-02-20 12:16:11 +0000

    Initial Git commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LevelController : MonoBehaviour {
+
+    private bool levelStart;
+    private float levelStartTime;
+    public TextMeshProUGUI timerText;
+    private float levelLimitTime;
+    private GameObject camel;
+    
+    // Use this for initialization
+    void Start()
+    {
+        levelLimitTime = 5;
+        levelStart = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update timer
+        if (levelStart)
+        {
+            float t = Time.time - levelStartTime;
+            if (t >= levelLimitTime)
+            {
+                levelStart = false;
+                timerText.text = "00:00";
+                timerText.color = Color.red;
+            }
+            else
+            {
+                t = levelLimitTime - t;
+                string minutes = Mathf.Floor(t / 60).ToString("00");
+                string seconds = (t % 60).ToString("00");
+               
+            
+                timerText.text = minutes + ":" + seconds;
+            }
+
+        }
+    }
+
+    public void StartTimer()
+    {
+        levelStart = true;
+        levelStartTime = Time.time;
+    }
+}
---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speedMultiplier;
+    private bool clickable;
+    Animator animator;
+    Vector3 direction;
+    public bool firstTime;
+    LevelController levelController;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        clickable = true;
+        animator.SetBool("isWalking", false);
+        firstTime = false;
+        levelController = GameObject.FindWithTag("GameController").GetComponent<LevelController>();
+
+    }
+
+    void OnMouseDown()
+    {
+        if (!firstTime)
+        {
+            firstTime = true;
+            levelController.StartTimer();
+        }
+
+        Debug.Log("Mouse Down");
+        if (clickable)
+        {
+            animator.SetBool("isWalking", true);
+            direction  = Vector3.right;
+            clickable = false;
+            Debug.Log("Clicked");
+        }
+    }
+
+ 
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        if (!clickable)
+        {
+            rb.velocity = direction * Time.deltaTime * speedMultiplier;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //When camel reaches the collider, it stops
+        rb.velocity = Vector3.zero;
+        animator.SetBool("isWalking", false);
+
+        //Rotate the camel
+        rb.rotation = rb.rotation * Quaternion.Euler(0, 180, 0);
+
+        //if camel hits the market
+        if(other.gameObject.name =="Market")
+        {
+            Debug.Log("It's market");
+            //Move camel back
+            direction = Vector3.left;
+            animator.SetBool("isWalking", true);
+
+        }
+        //if camel hits beginning marker
+        else
+        {
+            Debug.Log("Back at the start");
+            //let the user click the camel
+            clickable = true;
+       
+        }
+
+    }
+
+}
