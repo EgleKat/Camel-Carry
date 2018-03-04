b467ae6f5ef6de466ca8d4ac755333fcaa276748
Unknown <ek1g15@soton.ac.uk>
2018-03-04 15:44:51 +0000

    Can press 'R' only every 4 sex

--- Assets/Scripts/UserControlls.cs
+++ Assets/Scripts/UserControlls.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class UserControlls : MonoBehaviour {
+public class UserControlls : MonoBehaviour
+{
 
-    private bool instructedToRestart;
-	// Use this for initialization
-	void Awake () {
+    private bool instructedToRestart; float lastTime;
+
+    // Use this for initialization
+    void Awake()
+    {
         instructedToRestart = false;
         DontDestroyOnLoad(this);
     }
 
     void Start()
     {
+        lastTime = Time.time;
 
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && (Time.time - lastTime > 4.0f))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+            lastTime = Time.time;
 
+        }
     }
+
+
 
     public bool GetInstructedToRestart()
     {
